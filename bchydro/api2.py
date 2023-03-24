17807ed89ff5ab8c84757fc1d624b3e6840605b6
James McDonnell <topgamer7@gmail.com>
2023-03-24 02:04:24 -0700

    Convert to playright, hack in rest of api2.py

--- bchydro/api2.py
+++ bchydro/api2.py
@@ -1,5 +1,5 @@
 #import sys
-#import aiohttp
+import aiohttp
 import logging
 # from datetime import datetime, timedelta
 # import xml.etree.ElementTree as ET
@@ -14,7 +14,7 @@ import logging
 # )
 
 import asyncio
-from pyppeteer import launch
+from playwright.async_api import async_playwright
 import time
 import os
 import xml.etree.ElementTree as ET
@@ -66,31 +66,35 @@ class BCHydroApi2:
         
     def _authenticated(func):
         async def wrapper(self, *args, **kwargs):
-            if not (self.slid and self.page):
-                await self.authenticate()
-            await func(self, *args, **kwargs)
+            # if not (self.slid and self.page):
+            await self.authenticate()
+            return await func(self, *args, **kwargs)
         return wrapper
 
     async def authenticate(self):
-        self.browser = await launch(slowMo=False, autoclose=True, devtools=False)
-        self.page = await self.browser.newPage()
+        p = await async_playwright().__aenter__()
+        logger.debug('Launching firefox...')
+        self.browser = await p.firefox.launch()
+        self.page = await self.browser.new_page()
 
         logger.debug('Populating login form...')
         await self.page.goto(URL_LOGIN_PAGE)
-        await self.page.waitForSelector('#email')
+        await self.page.locator('#email').wait_for()
         await self.page.type('#email', self.username)
         await self.page.type('#password', self.password)
 
         logger.debug('Clicking login button...')
         await asyncio.gather(
-            self.page.waitForNavigation(),
+            # self.page.waitForNavigation(),
             self.page.click('#submit-button'),
         )
 
+        await self.page.locator("#billing_widget").wait_for()
         logger.debug('Extracting account numbers...')
-        self.slid = await self.page.evaluate("input_slid")
-        self.accountNumber = await self.page.evaluate("input_accountNumber")
-
+        self.slid = await self.page.evaluate("window.input_slid")
+        self.accountNumber = await self.page.evaluate("window.input_accountNumber")
+        print(self.accountNumber)
+        print(self.slid)
 
     @_authenticated
     async def get_accounts(self):
@@ -115,20 +119,30 @@ class BCHydroApi2:
     @_authenticated
     async def get_usage(self, hourly=False):
         # Navigate to Consumption page by clicking button:
+        # await self.page.wait_for_load_state()
+        # await self.page.locator('#detailCon:not([disabled])').wait_for()
+        # await asyncio.gather(
+        #     self.page.locator("#detailCon").wait_for(),
+        #     self.page.click('#detailCon'),
+        # )
+        # logger.debug('waiting Detailed Consumption button...')
+        # await self.page.locator("#detailCon:not([disabled])").wait_for()
         logger.debug('Clicking Detailed Consumption button...')
-        await self.page.waitForSelector('#detailCon:not([disabled])')
-        await asyncio.gather(
-            self.page.waitForNavigation(),
-            self.page.click('#detailCon'),
-        )
+        await self.page.click('#detailCon')
+        await self.page.wait_for_timeout(1000)
 
         # Evaluate JS fetch() request in DOM
         logger.debug('Extracting bchydroparam...')
-        bchp = await self.page.evaluate("document.querySelector('span#bchydroparam').innerText")
+        span = self.page.locator("span#bchydroparam")
+        bchp = await span.evaluate("span => span.innerText")
+        # bchp = await self.page.evaluate("document.querySelector('span#bchydroparam').innerText")
         url="https://app.bchydro.com/evportlet/web/consumption-data.html"
 
         logger.debug('Making fetch() request...')
-        postdata = f'Slid={self.slid}&Account={self.accountNumber}&ChartType=column&Granularity=daily&Overlays=none&StartDateTime=2022-04-07T00%3A00%3A00-07%3A00&EndDateTime=2022-06-07T00%3A00%3A00-07%3A00&DateRange=currentBill&RateGroup=RES1'
+
+        evpBillingStart = await self.page.evaluate("window.g_billingStartDateTime.toXmlDate()")
+        evpBillingEnd = await self.page.evaluate("window.g_billingEndDateTime.toXmlDate()")
+        postdata = f'Slid={self.slid}&Account={self.accountNumber}&ChartType=column&Granularity=daily&Overlays=none&StartDateTime={evpBillingStart}&EndDateTime={evpBillingEnd}&DateRange=currentBill&RateGroup=RES1'
         eval_js = f"""
         async()=>{{
             const res = await fetch(
@@ -150,4 +164,41 @@ class BCHydroApi2:
 
         xml = await self.page.evaluate(eval_js)
         usage, rates = parse_consumption_xml(xml)
+
+        self.usage = usage
+        # self._set_latest_point(usage)
+        self._set_latest_interval(usage)
+        self._set_latest_usage(usage)
+        self._set_latest_cost(usage)
         return usage, rates
+
+    def _is_valid_point(self, point):
+        return point.quality == "ACTUAL"
+
+    def _set_latest_point(self, usage):
+        valid_point = list(filter(self._is_valid_point, self.usage.electricity))
+        self.latest_point = valid_point[-1] if len(valid_point) else None
+
+    async def get_latest_point(self) -> BCHydroDailyElectricity:
+        return self.latest_point
+
+    def _set_latest_interval(self, usage):
+        valid_point = list(filter(self._is_valid_point, self.usage))
+        self.latest_interval = valid_point[-1].interval if len(valid_point) else None
+
+    async def get_latest_interval(self) -> BCHydroInterval:
+        return self.latest_interval
+
+    def _set_latest_usage(self, usage):
+        valid_point = list(filter(self._is_valid_point, self.usage))
+        self.latest_usage = valid_point[-1].consumption if len(valid_point) else None
+
+    async def get_latest_usage(self):
+        return self.latest_usage
+
+    def _set_latest_cost(self, usage):
+        valid_point = list(filter(self._is_valid_point, self.usage))
+        self.latest_cost = valid_point[-1].cost if len(valid_point) else None
+
+    async def get_latest_cost(self):
+        return self.latest_cost
